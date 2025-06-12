2eab90756a74bf17490f0f2388b361b1285a6fcc
hayakawakaki <hayakawakaki@gmail.com>
2025-06-12 23:54:44 +0900

    Updated fetch to a hook (#4)
    - Cleanup the app code by removing the fetching on the app and put it in a custom hook

--- src/components/HeroesGrid.tsx
+++ src/components/HeroesGrid.tsx
@@ -1,10 +1,21 @@
+import Loading from "@components/Loading";
 import type { HeroTypes } from "@/types/heroes";
 
 type HeroesGridProps = {
   heroData: HeroTypes[];
+  loading: boolean;
+  error: string | null;
 };
 
-function HeroesGrid({ heroData }: HeroesGridProps) {
+function HeroesGrid({ heroData, loading, error }: HeroesGridProps) {
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>Error : {error}</div>;
+  }
+
   return (
     <div>
       {heroData.map((item) => (
