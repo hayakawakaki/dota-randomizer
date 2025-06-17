fe432ff00b47688754eca106ec0dd9f8186690be
Hayakawa Kaki <hayakawakaki@gmail.com>
2025-06-17 15:02:49 +0900

    Added basic styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
+import "@css/reset.css";
+import "@css/typography.css";
+import "@css/variables.css";
+import "@css/default.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
