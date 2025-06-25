352102ab67fed3af53c79416852f5a8addc49bda
Hayakawa Kaki <hayakawakaki@gmail.com>
2025-06-25 15:46:49 +0900

    Corrected provider order in tree.

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,23 @@ import { DeviceProvider } from "@/hooks/device";
 
 import Layout from "@components/Layout";
 import Loading from "@components/Loading";
-import { useDevice } from "@/hooks/device/useDevice";
 import { HeroesComponent } from "@features/heroes";
 
 function App() {
-  const { isDeviceAtLeast } = useDevice();
   const queryClient = new QueryClient();
 
-  const isMobile = !isDeviceAtLeast("LAPTOP");
-
   return (
-    <Layout>
-      <ErrorBoundary fallback={<div>Failed to load heroes</div>}>
-        <Suspense fallback={<Loading />}>
-          <QueryClientProvider client={queryClient}>
-            <DeviceProvider>
+    <DeviceProvider>
+      <Layout>
+        <ErrorBoundary fallback={<div>Failed to load heroes</div>}>
+          <Suspense fallback={<Loading />}>
+            <QueryClientProvider client={queryClient}>
               <HeroesComponent />
-            </DeviceProvider>
-          </QueryClientProvider>
-        </Suspense>
-      </ErrorBoundary>
-    </Layout>
+            </QueryClientProvider>
+          </Suspense>
+        </ErrorBoundary>
+      </Layout>
+    </DeviceProvider>
   );
 }
 
